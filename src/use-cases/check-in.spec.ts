11c157ad3a3db4313f93464031d5e54e126b8980
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-06 10:50:26 -0300

    test: adding checkins use case tests

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { CheckinUseCase } from './check-in.js'
 import { InMemoryCheckInsRepository } from 'src/repositories/in-memory/in-memory-check-ins-repositorie.js'
 import { randomUUID } from 'node:crypto'
@@ -10,8 +10,15 @@ let sut: CheckinUseCase
 describe('Checkins use case', () => {
 
     beforeEach(() => {
-         checkinsRepository = new InMemoryCheckInsRepository()
-         sut = new CheckinUseCase(checkinsRepository)
+        checkinsRepository = new InMemoryCheckInsRepository()
+        sut = new CheckinUseCase(checkinsRepository)
+
+        vi.useFakeTimers()
+    })
+
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
     it('should be able to check in', async () => {
 
@@ -21,6 +28,41 @@ describe('Checkins use case', () => {
             userId: randomUUID()
         })
 
+        expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+
+    it('should not be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2022, 0, 1, 0, 0, 0))
+
+
+        await sut.execute({
+            gymId: randomUUID(),
+            userId: randomUUID()
+        })
+
+        await expect(() => sut.execute({
+            gymId: randomUUID(),
+            userId: randomUUID()
+        })).rejects.toBeInstanceOf(Error)
+    })
+
+    it('should be able to check in twice but in different days', async () => {
+        vi.setSystemTime(new Date(2022, 0, 1, 8, 0, 0))
+
+
+        await sut.execute({
+            gymId: randomUUID(),
+            userId: randomUUID()
+        })
+
+        vi.setSystemTime(new Date(2022, 0, 2, 8, 0, 0))
+
+        const { checkIn } = await sut.execute({
+            gymId: randomUUID(),
+            userId: randomUUID()
+        })
+         
         expect(checkIn.id).toEqual(expect.any(String))
     })
 })
