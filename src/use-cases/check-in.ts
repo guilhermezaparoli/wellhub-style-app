e825177fb5e54a1075b52c6af0c3f2a3539d1eaf
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-08 20:56:10 -0300

    feat: adding gymsRepository dependency in check-in usecase

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,26 +1,40 @@
 import { CheckIn } from 'generated/prisma/index.js';
 import { CheckinsRepository } from 'src/repositories/check-ins-repository.js';
+import { GymsRepository } from 'src/repositories/gyms-repository.js';
+import { ResourceNotFoundError } from './errors/resource-not-found.js';
 
 
 interface CheckinUseCaseRequest {
     userId: string
     gymId: string
+    userLatitude: number
+    userLongitude: number
 }
 interface CheckinUseCaseResponse {
     checkIn: CheckIn
 }
 export class CheckinUseCase {
 
-    constructor(private checkInsRepository: CheckinsRepository) { }
+    constructor(
+        private checkInsRepository: CheckinsRepository,
+        private gymsRepository: GymsRepository
+    ) { }
 
 
     async execute({ userId, gymId }: CheckinUseCaseRequest): Promise<CheckinUseCaseResponse> {
 
-        const checkInOnSameDay =  await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+        const gym = await this.gymsRepository.findById(gymId)
 
+        if(!gym){
+            throw new ResourceNotFoundError()
+        }
+
+        const checkInOnSameDay =  await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+        
         if(checkInOnSameDay){
             throw new Error()
         }
+        
 
         const checkIn = await this.checkInsRepository.create({
             gym_id: gymId,
