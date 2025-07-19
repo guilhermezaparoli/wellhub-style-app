e24bb1ceaf24d4611d8d40a7f05ba568ad4479b2
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-19 10:21:37 -0300

    feat: validate check-in to be greater than twenty minutus comparing with current date

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from 'generated/prisma/index.js';
 import { CheckinsRepository } from 'src/repositories/check-ins-repository.js';
 import { ResourceNotFoundError } from './errors/resource-not-found.js';
+import dayjs from 'dayjs';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error.js';
 
 
 interface ValidateCheckinUseCaseRequest {
@@ -24,6 +26,12 @@ export class ValidateCheckinUseCase {
 
         if (!checkIn) {
             throw new ResourceNotFoundError()
+        }
+
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(checkIn.created_at, 'minutes')
+
+        if(distanceInMinutesFromCheckInCreation > 20){
+            throw new LateCheckInValidationError()
         }
 
         checkIn.validated_at = new Date()
