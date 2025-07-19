b9c1674f3be8bb7fce95aebeb3eed445a6afcb00
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-07-19 10:45:45 -0300

    feat: implementing prismaCheckInsRepository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,10 +1,67 @@
 import { Prisma, CheckIn } from 'generated/prisma/index.js';
 import { CheckinsRepository } from '../check-ins-repository.js';
 import { prisma } from 'src/lib/prisma.js';
+import dayjs from 'dayjs';
 
 export class PrismaCheckInsRepositories implements CheckinsRepository {
+    async save(data: CheckIn) {
+        const checkIn = await prisma.checkIn.update({
+            where: {
+                id: data.id
+            },
+            data: data,
+        })
 
-    async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+        return checkIn
+    }
+    async findByUserIdOnDate(userId: string, date: Date) {
+        const startOfTheDay = dayjs(date).startOf('date')
+        const endOfTheDay = dayjs(date).endOf('date')
+
+        const checkIn = await prisma.checkIn.findFirst({
+            where: {
+                user_id: userId,
+                created_at: {
+                    gte: startOfTheDay.toDate(),
+                    lte: endOfTheDay.toDate()
+                }
+            }
+        })
+
+        return checkIn
+    }
+    async findManyByUserId(userId: string, page: number) {
+
+        const checkIns = await prisma.checkIn.findMany({
+            where: {
+                user_id: userId
+            },
+            take: 20,
+            skip: (page - 1) * 20
+
+        })
+        return checkIns
+    }
+    async getAmountCheckInsByUserId(userId: string) {
+        const count = await prisma.checkIn.count({
+            where: {
+                user_id: userId
+            }
+        })
+
+        return count
+    }
+    async findById(id: string) {
+        const checkIn = await prisma.checkIn.findUnique({
+            where: {
+                id
+            }
+        })
+
+        return checkIn
+    }
+
+    async create(data: Prisma.CheckInUncheckedCreateInput) {
 
         const checkIn = await prisma.checkIn.create({
             data
