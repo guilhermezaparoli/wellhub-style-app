e5a776fe58a38dd194dbbdf63422ce8785c379b5
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-08-26 08:56:29 -0300

    feat: creating nearby gyms controller

---
+++ src/http/controller/gyms/nearby.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+import { makeFetchNearbyGymsUseCase } from 'src/use-cases/factories/make-fetch-nearby-gyms-use-case.js';
+import { z } from 'zod';
+
+
+export async function nearby(req: FastifyRequest, res: FastifyReply) {
+
+    const nearbyGymsQuerySchema = z.object({
+        userLatitude: z.number().refine(value => {
+            return Math.abs(value) <= 90
+        }),
+        userLongitude: z.number().refine(value => {
+            return Math.abs(value) <= 180
+        }),
+    })
+
+    const { userLatitude, userLongitude } = nearbyGymsQuerySchema.parse(req.body)
+
+    const createGymUseCase = makeFetchNearbyGymsUseCase()
+
+    const { gyms } = await createGymUseCase.execute({
+        userLatitude,
+        userLongitude
+    })
+
+    return res.status(200).send({
+        gyms
+    })
+}
