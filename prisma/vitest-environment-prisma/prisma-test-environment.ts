5db58820a1af7b012cfa0f05b1094e0f2d848f32
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-08-26 07:39:55 -0300

    feat: creating vitest environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,43 @@
+
+import 'dotenv/config'
+import { PrismaClient } from 'generated/prisma/index.js';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest/environments';
+
+
+function generateUrlDatabase(schema: string) {
+
+    if(!process.env.DATABASE_URL){
+        throw new Error('Please provide a DATABASE_URL environment')
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schema)
+
+    console.log(url.searchParams);
+
+    return url.toString()
+}
 
 export default <Environment> {
     name: 'prisma',
     transformMode: 'web',
     async setup() {
-        console.log('teste');
+        const prisma = new PrismaClient()
+
+        const schema = randomUUID()
+        const databaseUrl = generateUrlDatabase(schema)
+
+        process.env.DATABASE_URL = databaseUrl
+
+        execSync('npx prisma migrate deploy')
         return {
-           async teardown() {}
+           async teardown() {
+            await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+            await prisma.$disconnect()
+           }
         }
     }
 }
