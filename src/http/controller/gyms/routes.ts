2927804534e552f158e42062717ba54d8028ec6d
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-08-26 08:57:15 -0300

    feat: adding controller routes

--- src/http/controller/gyms/routes.ts
+++ src/http/controller/gyms/routes.ts
@@ -1,9 +1,20 @@
 import { FastifyInstance } from 'fastify';
 
 import { verifyJWT } from '../../middlewares/verify-jwt.js';
+import { create } from './create.js';
+import { nearby } from './nearby.js';
+import { search } from './search.js';
 
 
 export async function gymsRoutes(app: FastifyInstance) {
 
-   app.addHook('onRequest', verifyJWT)
+    app.addHook('onRequest', verifyJWT)
+
+
+
+    app.get('/gyms/search', search)
+    app.get('/gyms/nearby', nearby)
+    
+    app.get('/gyms', create)
+
 }
