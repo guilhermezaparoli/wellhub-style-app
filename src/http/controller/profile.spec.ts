cb6739f0fd5a26a755ddac26b9e5576de0ea303f
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-08-26 08:00:42 -0300

    teste: profile e2e

---
+++ src/http/controller/profile.spec.ts
@@ -0,0 +1,36 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest'
+import { app } from '../../app.js';
+describe('Profile (e2e)', async () => {
+
+    beforeAll(async () => {
+        app.ready()
+    })
+    afterAll(async () => {
+        app.close()
+    })
+
+
+    it('Should be able to get user profile', async () => {
+        await request(app.server).post('/users').send({
+            name: 'Guilherme',
+            email: 'guilhermeteste@gmail.com',
+            password: '1234567'
+        })
+
+        const authResponse = await request(app.server).post('/sessions').send({
+            email: 'guilhermeteste@gmail.com',
+            password: '1234567'
+        })
+        const { token } = authResponse.body
+
+
+        const profileResponse = await request(app.server).get('/me').set('Authorization', `Bearer ${token}`)
+
+
+        expect(profileResponse.statusCode).toBe(200);
+        expect(profileResponse.body.user).toEqual(expect.objectContaining({
+            email: 'guilhermeteste@gmail.com'
+        }))
+    })
+})
